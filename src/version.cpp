e5ec044ab8c4b56e0922bead2f57367cbf229f34
freakcoderz <42624259+freakcoderz@users.noreply.github.com>
2021-02-17 09:20:47 -0500

    Version 2 updates
    Image changes, add background and remove unneeded duplicate files.

--- src/version.cpp
+++ src/version.cpp
@@ -36,7 +36,7 @@ const std::string CLIENT_NAME("Mousecoin");
 // git will put "#define GIT_ARCHIVE 1" on the next line inside archives. 
 #define GIT_ARCHIVE 1
 #ifdef GIT_ARCHIVE
-#    define GIT_COMMIT_ID ""
+#    define GIT_COMMIT_ID "-freak"
 #    define GIT_COMMIT_DATE "$Format:%cD"
 #endif
 
